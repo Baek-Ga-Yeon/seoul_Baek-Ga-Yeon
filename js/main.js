b1ebd1b2a857b627d191440af36219b949aabbb4
Baek-Ga-Yeon <90228141+Baek-Ga-Yeon@users.noreply.github.com>
2021-10-27 16:51:06 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+// IF 조건문
+var a = 20;
+var b = 40;
+var c = 60;
+
+/*
+if(a > b) {
+    console.log("a는 b보다 작다.");
+}
+*/
+
+
+
+/*
+if(a > b) {
+    console.log("a는 b보다 크다.");
+} else {
+    console.log("a는 b보다 작다.")
+}
+*/
+
+
+
+/*
+
+if(a > b) {
+    console.log("a는 b보다 크다.");
+} else if(b > c) {
+    console.log("b는 c보다 크다.");
+} else if(a < c) {
+    console.log("a는 c보다 작다.");
+} else if(b < c) {
+    console.log("b는 c보다 작다.");
+} else {
+    console.log("모든 조건을 만족하지 않는다.");
+}
+
+*/    
+
+
 
 // comic 박스를 생성한 상태
 // 변수선언
@@ -111,6 +151,7 @@ console.log(house);
 // var msg3 = "Nice to meet you';";
 
 
+
 // She is girl
 // She's girl
 //var msg4 = "She's girl";
@@ -128,6 +169,248 @@ console.log(house);
 
 
 // 숫자 (Number)
+
+
+/*
+var num1 = 10;
+var num2 = -10;
+var num3 = 3.14;
+var num4 = -3.14;
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+
+console.log(num1 + num2);
+console.log(num1 + num4);
+*/
+
+
+// 연산자
+// 숫자 사칙연산
+//var a = 20;
+//var a = 10;
+
+
+//console.log(str1 + str2);
+//console.log(str1 - str2);
+//console.log(str1 * str2);
+//console.log(str1 / str2);
+//console.log(str1 % str2);
+
+
+
+
+//var firstName = "Inkweon";
+//var lastName = "Kim";
+
+
+
+
+
+//console.log(firstName + "     " +lastName);
+
+
+// answkduf (String)
+
+
+
+
+//var msg1 = "Hello World";
+//var msg2 = 'Wecome';
+
+
+//console.log(msg1);
+//console.log(msg2);
+
+// var msg3 = "Nice to meet you';";
+
+
+
+// She is girl
+// She's girl
+//var msg4 = "She's girl";
+//var msg5 = 'She\'s girl';
+
+
+
+
+//console.log(msg4);
+//console.log(msg5);
+
+
+
+
+
+
+// ++, -- 연산자
+//var mum10 = 10;
+
+
+
+
+//num10 = num10 - 1;
+//console.log(num10);
+// console.log(--num10); // 8
+// console.log(--num10); // 9
+
+
+
+
+//console.log(++num10);
+//console.log(++num10);
+
+
+
+//console.log(--num10);
+//console.log(num10);
+
+
+
+
+
+
+
+
+/*
+
+// +=, _+, *=, /=, %=
+var num20 = num20 + 10;
+num20 += 10;
+
+console.log(num20);
+
+
+/*
+
+
+
+
+// 비교 연산자 >, <, ==, !=, !==, ===, <==, >=
+
+var a =10;
+var b =20;
+var c =30;
+
+
+/* 
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+
+
+
+console.log(a == b);
+console.log(a === b);
+
+
+
+
+console.log(a != b);
+console.log(a !== b);
+
+*/
+
+
+
+/*
+
+var num10 = 10;
+var num20 = 20;
+
+var str10 = "10";
+var str20 = "20";
+
+
+console.log(num10 == str10);
+console.log(num10 === str10);
+
+console.log(num20 != str20);
+console.log(num20 !== str20);
+
+/*
+
+
+// 논리 연산자
+
+var num30 = 30;
+var num40 = 40;
+var num50 = 50;
+
+
+console.log(num30 === num40 && num30 === num50 && num40 !== num50);
+console.log(num30 === num40 || num30 === num50 || num40 > num50);
+
+
+
+// 불리언(boolean) : true, false
+
+console.log(true);
+console.log(false);
+console.log(1 === 2);
+console.log(10 < 20);
+console.log(10 === 10 || 20 === 30);
+
+var bool = true;
+bool = false;
+
+// answkduf (String)
+
+
+
+
+//var msg1 = "Hello World";
+//var msg2 = 'Wecome';
+
+
+//console.log(msg1);
+//console.log(msg2);
+
+// var msg3 = "Nice to meet you';";
+
+
+// She is girl
+// She's girl
+//var msg4 = "She's girl";
+//var msg5 = 'She\'s girl';
+
+
+
+
+//console.log(msg4);
+//console.log(msg5);
+
+
+
+
+
+
+// 숫자 (Number)
+
+
+/*
+var num1 = 10;
+var num2 = -10;
+var num3 = 3.14;
+var num4 = -3.14;
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+
+console.log(num1 + num2);
+console.log(num1 + num4);
+*/
+
 
 
 /*
@@ -280,7 +563,59 @@ console.log(num30 === num40 || num30 === num50 || num40 > num50);
 
 
 
-// 불리언(boolean) : true, false
+console.log(true);
+console.log(false);
+console.log(1 === 2);
+console.log(10 < 20);
+console.log(10 === 10 || 20 === 30);
+
+var bool = true;
+bool = false;
+
+
+
+var arr = ["사과", "배", "수박"];
+
+// console.log(arr.length);
+
+// arr. push("포도", "토마토");
+// console.log(arr);
+
+
+// arr.unshift("포도");
+// arr.unshift("토마토");
+// console.log(arr);
+
+
+/*
+arr.pop();
+arr.pop();
+
+console.log(arr);
+
+*/
+
+//arr.shift();
+
+//console.log(arr);
+
+
+
+
+
+//var arr1 = ["사과", "배", "바나나"];
+//var arr1 = ["노트북", "마이크", "키보드"];
+
+//var str1 = arr1.join(' / ');
+//console.log(str1);
+
+
+
+var merge = arr1.concat(arr2);
+console.log(merge);
+
+
+
 
 console.log(true);
 console.log(false);
@@ -290,3 +625,103 @@ console.log(10 === 10 || 20 === 30);
 
 var bool = true;
 bool = false;
+
+var arr = ["사과", "배", "수박"];
+
+// console.log(arr.length);
+
+// arr. push("포도", "토마토");
+// console.log(arr);
+
+
+// arr.unshift("포도");
+// arr.unshift("토마토");
+// console.log(arr);
+
+
+/*
+arr.pop();
+arr.pop();
+
+console.log(arr);
+
+*/
+
+
+//arr.shift();
+
+//console.log(arr);
+
+
+
+
+var arr1 = ["사과", "배", "바나나"];
+var arr1 = ["노트북", "마이크", "키보드"];
+
+//var str1 = arr1.join(' / ');
+//console.log(str1);
+
+
+
+
+
+var merge = arr1.concat(arr2);
+console.log(merge);
+
+
+
+
+
+
+
+
+
+
+var hex = document.getElementById('hex');
+var btn = document.getElementById('btn');
+
+// console.log(hex);
+// console.log(btn);
+
+btn.addEventListener('click', createColor);
+
+
+function createColor() {
+
+    // console.log("클릭");
+    
+    
+    var arr = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f" ];
+    var color = '#'
+
+    
+    for(var i = 0; i < 6; i++) {
+        var random = Math.floor(Math.random() * arr.length);
+        color += arr[random];
+    }
+    
+    
+    document.body.style.backgroundColor = color;
+    hex.textContent = color;
+
+}
+
+
+
+
+// #000ffff;
+// 숫자 : 0 ~ 9
+// 알파벳 : a ~ f
+// 6글자 숫자와 알파벳 조합
+
+
+
+// # + 0 = #0
+// #0 + f = #0f;
+// #0f + a = #0fa;
+// #0fa + a = #0faa;
+// #0faa + b = #0faab;
+// #0faab + c = #0faabc;
+
+
+
